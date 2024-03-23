c5c4e7b7caf69d85b4191cafc84694a3ba3ea3d8
EnoshLi <1530217461@qq.com>
2024-03-23 19:19:15 +0800

    卡牌拖拽

---
+++ Assets/Scritps/Card/MonoBehaviour/CardDragHandler.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDragHandler : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public bool canMove;
+    public Card currentCard;
+    public bool canExecute;
+
+    private void Awake()
+    {
+        currentCard = GetComponent<Card>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        switch (currentCard.cardDataSo.cardType)
+        {
+            case CardType.Attack:
+                break;
+            case CardType.Defense:
+            case CardType.Abilities:
+                canMove = true;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (canMove)
+        {
+            currentCard.isAnimating = true;
+            Vector3 screenPos = new(Input.mousePosition.x,Input.mousePosition.y,10);
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+            currentCard.transform.position = worldPos;
+            //执行条件
+            canExecute = worldPos.y > 0.1f;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (canExecute)
+        {
+            
+        }
+        //throw new System.NotImplementedException();
+        currentCard.RestCardTransform();
+        currentCard.isAnimating = false;
+    }
+}
